5cbe85c086137d47a717858e636585bcb424b0cb
Yuri Martinovsky <vvrite@me.com>
2021-06-17 12:46:35 +0300

    added icons custom cell

---
+++ exampleUITableView/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  exampleUITableView
+//
+//  Created by Yuriy Martinovskiy on 17.06.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+
+}
